7b358745f496e34592e316aecfce875dd7fc564f
Latsuj <justal.kevin@gmail.com>
2020-07-29 20:13:11 +0800

    [REFACTORING] Changing the name of the function, chaning the ordering of the code, changing the place of the code, adding new pure function

--- index.js
+++ index.js
@@ -10,66 +10,87 @@ const options = {
 	upvotes: '.votesUp',
 	downvotes: '.votesDown',
 	percent: '.percent',
-	date: '',
 	author: '.video-detailed-info .usernameBadgesWrapper a',
 	pornstars: '.pornstarsWrapper .pstar-list-btn',
 	categories: '.categoriesWrapper a:not(.add-btn-small)',
 	tags: '.tagsWrapper a:not(.add-btn-small)',
 	production: '.productionWrapper',
-	description: 'meta[property="og:description"]',	
+	description: 'meta[property="og:description"]',
 	image: 'meta[property="og:image"]',
 	duration: 'meta[property="video:duration"]'
 }
 
-const parseDom = (doc,keys) => {
-	if(!keys || keys.length === 0) return {}
-	let datas = {};
-
+const scraper_content_informations = function(doc,keys) {
+	const rsl = {};
 	Object.keys(options).filter(option => keys.includes(option)).map(x => {
 		let elm = Array.from(doc.querySelectorAll(options[x]));
 		if(!elm || elm.length===0) return datas[x] = "No data";
-		
+
 		elm = elm.length===1 ? elm[0].textContent : elm.map(node => node.textContent);
-		
-		datas[x] = elm;
+
+		rsl[x] = elm;
 	})
-	
-	datas["uploaddate"] = JSON.parse(window.document.querySelectorAll('script[type="application/ld+json"')[0].textContent).uploadDate;
-	
+	return rsl;
+}
+
+const scraper_javascript_informations = function(doc,keys) {
+	const rsl = {};
+
+	if(keys.includes("upload_date")) rsl["upload_date"] = JSON.parse(doc.querySelectorAll('script[type="application/ld+json"')[0].textContent).uploadDate;
+
+	return rsl;
+}
+
+const scraper_video_informations = function(source,keys) {
+		const rsl = {};
+
+		if(keys.includes("download_urls")) {
+	    const matches = source.match(/(?<=\*\/)\w+/g), urls = [];
+	    for (let index = 0; index < matches.length; index++) {
+
+	        let regex = new RegExp('(?<=' + matches[index] + '=")[^;]+(?=")', "g");
+	        let value = source.match(regex)[0].replace(/[" + "]/g, "");
+
+	        if (value.startsWith("https")) {
+	            if (urls.length === 4) break;
+	            urls.push(value);
+	        } else urls[urls.length -1] += value;
+	    }
+
+			urls.map(x => rsl[x.match(/(?<=_)\d*P(?=_)/g)[0]] = x)
+		}
+
+    return rsl;
+}
+
+const parseDom = (source,keys) => {
+	const dom = new JSDOM(source);
+	const doc = dom.window.document;
+
+	if(!keys || keys.length === 0) return {}
+	let datas = {};
+
+	datas = {...datas,...scraper_content_informations(doc,keys)};
+	datas = {...datas,...scraper_javascript_informations(doc,keys)};
+	datas = {...datas,...scraper_video_informations(source,keys)};
+
 	return datas;
 };
-
-const getDownloadUrl = function(doc) {
-    const matches = doc.match(/(?<=\*\/)\w+/g), urls = []; 
-    for (let index = 0; index < matches.length; index++) { 
-        
-        let regex = new RegExp('(?<=' + matches[index] + '=")[^;]+(?=")', "g");
-        let value = doc.match(regex)[0].replace(/[" + "]/g, "");
-        
-        if (value.startsWith("https")) {
-            if (urls.length === 4) break;
-            urls.push(value);
-        } else urls[urls.length -1] += value;
-    }
-    
-    return urls;
-}
 
 module.exports = {
 	"page" : async (url, key) => {
 		const keys = Array.isArray(key) ? key : [key];
-	
+
 		try {
 			const response = await got(url);
 			const source = response.body;
-			const dom = new JSDOM(source);
-			return parseDom(dom.window.document,keys);
+			return parseDom(source,keys);
 		} catch (error) {
 			console.log(error);
 			if (error) {
 				error.message = 'the requested data is not available';
 			}
-	
+
 			return {data: error.message};
 	}
 }
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ import m from '.';
 const url = 'https://www.pornhub.com/view_video.php?viewkey=ph56fc59c124c0c';
 
 test('title', async t => {
-	const video = await m.page(url, ['title','description','views','upvotes','downvotes','percent','author','pornstars','categories','tags']);
+	const video = await m.page(url, ['title','description','views','upvotes','downvotes','percent','author','pornstars','categories','tags','upload_date','download_urls']);
 	console.log(video);
 	t.is(video.title, 'Hot Kissing Scene');
 });
