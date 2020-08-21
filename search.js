d9fb37b2a8ed387c9028bec5d3f919f44780b9e3
Latsuj <justal.kevin@gmail.com>
2020-08-21 13:10:16 +0800

    [REFACTORING] Removing the custom scrapper inside search

--- search.js
+++ search.js
@@ -3,27 +3,13 @@ const constants = require('./consts');
 const jsdom = require('jsdom');
 const {JSDOM} = jsdom;
 
-const scraper_search_content_informations = (doc, keys) => {
-	const rsl = {};
-
-	const videos = [...doc.querySelectorAll(constants.global_selectors.SEARCH_LIST)];
-	let obj_videos = videos.map((video,index) => {
-        const tmp = utils.scrap(video,constants.secondary_search_selectors,constants.page_search_element_attributs);
-        return utils.sanitizer(tmp);
-	})
-	rsl[constants.keys.RESULTS] = obj_videos;
-
-	return rsl;
-}
-
-
 module.exports = {
     scraping_search: (source, keys) => {
     	const doc = utils.source_to_dom(source);
 
         let datas = {};
 		datas = {...datas, ...utils.scraper_content_informations(doc, keys, constants.primary_search_selectors ,constants.page_search_element_attributs)};
-		datas = {...datas, ...scraper_search_content_informations(doc, keys)};
+		datas = {...datas, ...{constants.keys.RESULTS: utils.scraper_array(doc, constants.global_selectors.SEARCH_LIST, constants.secondary_search_selectors, constants.page_search_element_attributs)}};
 
         return datas;
     }
