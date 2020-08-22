31c482ab870decab7016466fd5ed982d7dbc29ab
Latsuj <justal.kevin@gmail.com>
2020-08-22 12:42:44 +0800

    [LINTER] Fixing the error of search.js

--- search.js
+++ search.js
@@ -4,13 +4,13 @@ const jsdom = require('jsdom');
 const {JSDOM} = jsdom;
 
 module.exports = {
-    scraping_search: (source, keys) => {
-    	const doc = utils.source_to_dom(source);
+	scraping_search: (source, keys) => {
+		const doc = utils.source_to_dom(source);
 
-        let datas = {};
-		datas = {...datas, ...utils.scraper_content_informations(doc, keys, constants.primary_search_selectors ,constants.page_search_element_attributs)};
+		let datas = {};
+		datas = {...datas, ...utils.scraper_content_informations(doc, keys, constants.primary_search_selectors, constants.page_search_element_attributs)};
 		datas[constants.keys.RESULTS] = utils.scraper_array(doc, constants.global_selectors.SEARCH_LIST, constants.secondary_search_selectors, constants.page_search_element_attributs);
 
-        return datas;
-    }
-}
+		return datas;
+	}
+};
