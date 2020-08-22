9a6d4738085c4a1333976dcee0c8d200c9321519
Latsuj <justal.kevin@gmail.com>
2020-08-22 12:54:29 +0800

    [LINTER] Fixing the error of index.js

--- index.js
+++ index.js
@@ -7,49 +7,52 @@ const page_search = require('./search');
 const promise = require('promise');
 const got = require('got');
 
-//TODO Search by country
+const options_to_keys = key => {
+	if (!key) {
+		return [];
+	}
 
-const options_to_keys = (key) => {
-	if(!key) return [];
 	const array_keys = Array.isArray(key) ? key : [key];
 	return array_keys.map(x => x.toUpperCase());
-}
+};
 
-const get_queries_from_keys = (keys) => {
+const get_queries_from_keys = keys => {
 	return Object.fromEntries(Object.keys(constants.queries).map(query => {
-		const key = keys.find(key => key.includes(query))
-		if(key) {
-			return [constants.queries[query], utils.sanitizer_number(key.replace(query,""))]
+		const key = keys.find(key => key.includes(query));
+		if (key) {
+			return [constants.queries[query], utils.sanitizer_number(key.replace(query, ''))];
 		}
+
 		return [];
 	}).filter(x => x));
-}
+};
 
-const url_to_source = async (url) => {
+const url_to_source = async url => {
 	const safe_url = url.toLowerCase();
 	const response = await got(safe_url);
 	return response.body;
-}
+};
 
 const multi_url_to_source = async (url, queries) => {
-	return await promise.all([...Array(queries.PAGE)].map(async (page,index) => {
-		return await url_to_source(constants.links.SEARCH+url+'&page='+(index+1));
-	}))
-}
+	return promise.all([...new Array(queries.PAGE)].map(async (page, index) => {
+		return url_to_source(constants.links.SEARCH + url + '&page=' + (index + 1));
+	}));
+};
 
-const error_message = (error) => {
-	console.log(error);
+const error_message = error => {
 	if (error) {
 		error.message = constants.errors.DEFAULT;
 	}
 
 	return {data: error.message};
-}
+};
 
 module.exports = {
 	page: async (url, key) => {
 		const keys = options_to_keys(key);
-		if(key.length === 0) return {};
+		if (key.length === 0) {
+			return {};
+		}
 
 		try {
 			const source = await url_to_source(url);
@@ -62,7 +65,9 @@ module.exports = {
 	search: async (search, key) => {
 		const keys = options_to_keys(key);
 		const queries = get_queries_from_keys(keys);
-		if(!queries.PAGE) queries['PAGE'] = 1;
+		if (!queries.PAGE) {
+			queries.PAGE = 1;
+		}
 
 		try {
 			const source = await multi_url_to_source(search, queries);
