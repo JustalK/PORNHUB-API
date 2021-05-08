1fcfb691f1ffd56bb153092662b9198e353a2a1e
Jaco <jaco@Jacos-MacBook-Pro.local>
2021-05-08 22:59:20 +0100

    Add filters for LONGEST and NEWEST

--- src/constants/consts_queries.js
+++ src/constants/consts_queries.js
@@ -2,7 +2,9 @@ module.exports = {
 	filter: {
 		MOST_VIEWED: 'mv',
 		HOTTEST: 'ht',
-		TOP_RATED: 'tr'
+		TOP_RATED: 'tr',
+		LONGEST: 'lg',
+		NEWEST: 'cm'
 	},
 	min_duration: [10, 20, 30],
 	max_duration: [10, 20, 30],
